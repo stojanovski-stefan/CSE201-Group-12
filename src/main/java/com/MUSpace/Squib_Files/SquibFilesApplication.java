7ec04787853114e207f8ca03bd4fd909abc3266a
hamiltka <161067183+hamiltka@users.noreply.github.com>
2024-03-05 13:02:03 -0500

    Add Spring Boot Files

---
+++ src/main/java/com/MUSpace/Squib_Files/SquibFilesApplication.java
@@ -0,0 +1,13 @@
+package com.MUSpace.Squib_Files;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquibFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquibFilesApplication.class, args);
+	}
+
+}

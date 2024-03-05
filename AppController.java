4f0344a684abc4412ec7b2cd228a563e1e3296ba
hamiltka <161067183+hamiltka@users.noreply.github.com>
2024-03-05 10:30:17 -0500

    SpringBackend: User Controller
    AppController: is the handler for mapping the User data

---
+++ AppController.java
@@ -0,0 +1,49 @@
+/*
+ * This is a handler method
+ * @author Kimberly Hamilton
+ */
+
+package net.codejava;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+ 
+@Controller
+public class AppController {
+ 
+    @Autowired
+    private UserRepository userRepo;
+     
+    @GetMapping("")
+    public String viewHomePage() {
+        return "index";
+    }
+    
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+         
+        return "signup_form";
+    }
+    
+    @PostMapping("/process_register")
+    public String processRegister(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+         
+        userRepo.save(user);
+         
+        return "register_success";
+    }
+    
+    @GetMapping("/users")
+    public String listUsers(Model model) {
+        List<User> listUsers = userRepo.findAll();
+        model.addAttribute("listUsers", listUsers);
+         
+        return "users";
+    }
+}
+

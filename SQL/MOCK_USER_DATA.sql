efbaaa6ab072e956c5064302b366025103a0baf5
Cameron Paul <paulcj3@miamioh.edu>
2024-03-14 13:36:36 -0400

    Changed INSERT statements to include random emails

--- SQL/MOCK_USER_DATA.sql
+++ SQL/MOCK_USER_DATA.sql
@@ -4,15 +4,15 @@ create table MOCK_USER_DATA (
 	last_name VARCHAR(50),
 	password VARCHAR(50),
 	username VARCHAR(50),
-    email VARCHAR(50)
+	email VARCHAR(50)
 );
-insert into MOCK_USER_DATA (id, first_name, last_name, password, username) values (1, 'Truman', 'Wands', 'f7@U_~pZ*s$', 'twands0');
-insert into MOCK_USER_DATA (id, first_name, last_name, password, username) values (2, 'Jinny', 'Joy', 'i0?\pZrK', 'jjoy1');
-insert into MOCK_USER_DATA (id, first_name, last_name, password, username) values (3, 'Aymer', 'Greeding', 'l2@>NPWxX!', 'agreeding2');
-insert into MOCK_USER_DATA (id, first_name, last_name, password, username) values (4, 'Eudora', 'Frosch', 'o8?W33PWiF6N', 'efrosch3');
-insert into MOCK_USER_DATA (id, first_name, last_name, password, username) values (5, 'Alexandre', 'Ibbeson', 'r2"WQ*IQ', 'aibbeson4');
-insert into MOCK_USER_DATA (id, first_name, last_name, password, username) values (6, 'Hal', 'Duly', 'd5"Mdqmq', 'hduly5');
-insert into MOCK_USER_DATA (id, first_name, last_name, password, username) values (7, 'Hermina', 'Knapman', 'a2|Kvj6+0', 'hknapman6');
-insert into MOCK_USER_DATA (id, first_name, last_name, password, username) values (8, 'Kelsy', 'Dionisio', 'z5~Ui$`''', 'kdionisio7');
-insert into MOCK_USER_DATA (id, first_name, last_name, password, username) values (9, 'Olivette', 'Aveling', 'b9&htd"AEI6<7X,@', 'oaveling8');
-insert into MOCK_USER_DATA (id, first_name, last_name, password, username) values (10, 'Noll', 'Muzzall', 'h7`c?5M|y#\X=mn', 'nmuzzall9');
+insert into MOCK_USER_DATA (id, first_name, last_name, password, username, email) values (1, 'Marcie', 'Pendered', 'c4*=+YSO"6)O!GE{', 'mpendered0', 'mpendered0@digg.com');
+insert into MOCK_USER_DATA (id, first_name, last_name, password, username, email) values (2, 'Shay', 'Biagioni', 'f4?_WFD!zAh', 'sbiagioni1', 'sbiagioni1@sina.com.cn');
+insert into MOCK_USER_DATA (id, first_name, last_name, password, username, email) values (3, 'Marlon', 'Purton', 't2?a=''t6E', 'mpurton2', 'mpurton2@alibaba.com');
+insert into MOCK_USER_DATA (id, first_name, last_name, password, username, email) values (4, 'Hayward', 'Ivain', 'k8!VK7CC1I', 'hivain3', 'hivain3@irs.gov');
+insert into MOCK_USER_DATA (id, first_name, last_name, password, username, email) values (5, 'Barbabra', 'Zylberdik', 'x6}ej{4bmFyG', 'bzylberdik4', 'bzylberdik4@usatoday.com');
+insert into MOCK_USER_DATA (id, first_name, last_name, password, username, email) values (6, 'Enid', 'McComas', 'z3{"Gy/''', 'emccomas5', 'emccomas5@geocities.com');
+insert into MOCK_USER_DATA (id, first_name, last_name, password, username, email) values (7, 'Katee', 'Senner', 'l2*2dE`vm', 'ksenner6', 'ksenner6@bandcamp.com');
+insert into MOCK_USER_DATA (id, first_name, last_name, password, username, email) values (8, 'Raynard', 'Boecke', 'b7*7+tNEQi', 'rboecke7', 'rboecke7@huffingtonpost.com');
+insert into MOCK_USER_DATA (id, first_name, last_name, password, username, email) values (9, 'Dun', 'Houseley', 'f7{Dbzh4', 'dhouseley8', 'dhouseley8@jiathis.com');
+insert into MOCK_USER_DATA (id, first_name, last_name, password, username, email) values (10, 'Wendall', 'Drackford', 'q8,4D54aP?9Lg`', 'wdrackford9', 'wdrackford9@cnn.com');
